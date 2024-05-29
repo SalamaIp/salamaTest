f14c532c9d246677b48b3d6324aa6e6f0d8ba665
salamaip <mohamed.salama.abdelhakim@gmail.com>
2024-05-29 13:05:54 +0500

    added database backup

--- Backend/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e207085f7240ce2be06c20128b1d8872423e55ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36dd802fcffa833af890f0a9cb0fbbf489bf856f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
